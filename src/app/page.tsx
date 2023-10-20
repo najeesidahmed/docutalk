89367d3097400e144b4ca86e0585ee51aebac123
najee sidahmed <najeekamil@msn.com>
2023-10-20 19:40:29 +0100

    add utils and max width component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import MaxWidthWrapper from "src/components/MaxWidthWrapper"
 
 export default function Home() {
   return (
-    <div>s</div>
+    <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-ft items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        
+      </div>
+    </MaxWidthWrapper>
+      
+    
   )
 }
